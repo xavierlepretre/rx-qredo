2d9ad6456af77bb7b7303fbfd4070a408e74a09a
Xavier Lepretre <xavier.lepretre@gmail.com>
2016-03-03 18:26:03 +0000

    Added further code examples.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Helper methods to facilitate the use of Qredo methods in an Rx workflow.
 
-## Vault Usage:
+## Getting a Connection:
 ### Get a `QredoClient`
 ```Java
 Observable<QredoClient> clientObservable = new QredoClientObservable(new QredoClientAdapter())
@@ -12,8 +12,10 @@ Observable<QredoClient> clientObservable = new QredoClientObservable(new QredoCl
         "USER SECRET",
         this); // Context
 ```
-It is your responsibility to `.unsubscribe()`, to unbind, in order to avoid memory leakage.
+Returns a single `QredoClient`, but never calls `.onCompleted()`.
+It is your responsibility to call `subscription.unsubscribe()`, as this triggers the `unbind` method, in order to avoid memory leakage.
 
+## Vault Usage:
 ### Put a `VaultItem`
 ```Java
 Observable<VaultItemRef> itemObservable = new VaultManagerObservable(qredoClient.getVaultManager())
@@ -73,4 +75,144 @@ Returns a single `Set`.
 Observable<VaultItemHeader> liveVaultItemUpdates = new VaultManagerObservable(qredoClient.getVaultManager())
     .listen();
 ```
-It is your responsibility to `.unsubscribe()` in order to avoid memory leakage.
+Returns 0 or more `VaultItem`, but never calls `.onCompleted()`.
+It is your responsibility to call `subscription.unsubscribe()` , as this triggers the `removeListener` method, in order to avoid memory leakage.
+
+## Rendezvous Usage:
+### Create a `Rendezvous`
+```Java
+Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+    .create(params);
+```
+Returns a single `Rendezvous`.
+
+### Respond to a `Rendezvous`
+ ```Java
+Observable<ConversationRef> conversationRefObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+     .respond(tag);
+ ```
+ Returns a single `ConversationRef`.
+
+ ### Get a `Rendezvous`
+ ```Java
+Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+     .get(ref);
+ ```
+ Returns a single `Rendezvous`.
+
+ ### Activate a `Rendezvous`
+ ```Java
+Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+     .activate(ref, duration);
+```
+Returns a single `Rendezvous`.
+
+### Deactivate a `Rendezvous`
+```Java
+Observable<Boolean> resultObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+    .deactivate(ref);
+```
+Returns a single `Boolean`.
+
+### List `Rendezvous`
+```Java
+Observable<Set<Rendezvous>> rendezvousesObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+    .list();
+```
+Returns a single `Set`.
+
+### Listen to live new `Rendezvous`
+```Java
+Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+    .listen();
+```
+Returns 0 or more `Rendezvous`, but never calls `.onCompleted()`.
+It is your responsibility to call `subscription.unsubscribe()` , as this triggers the `removeListener` method, in order to avoid memory leakage.
+
+## Conversation Usage:
+### List `Conversation` by `RendezvousRef`
+```Java
+Observable<Set<Conversation>> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .list(ref);
+```
+Returns a single `Set`.
+
+### List `Conversation` by Tag
+```Java
+Observable<Set<Conversation>> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .list(tag);
+```
+Returns a single `Set`.
+
+### List All `Conversation`
+```Java
+Observable<Set<Conversation>> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .listAll();
+```
+Returns a single `Set`.
+
+### Get a `Conversation`
+```Java
+Observable<Conversation> conversationObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .get(ref);
+```
+Returns a single `Conversation`.
+
+### Delete a `Conversation`
+```Java
+Observable<Boolean> resultObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .delete(ref);
+```
+Returns a single `Boolean`.
+
+### Leave a `Conversation`
+```Java
+Observable<Boolean> resultObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .leave(ref);
+```
+Returns a single `Boolean`.
+
+### Listen to live created `Conversation`
+```Java
+Observable<Conversation> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+    .listen(ref);
+```
+Returns 0 or more `Conversation`, but never calls `.onCompleted()`.
+It is your responsibility to call `subscription.unsubscribe()` , as this triggers the `removeListener` method, in order to avoid memory leakage.
+
+## ConversationMessage Usage:
+## Get a `ConversationMessage`
+```Java
+Observable<ConversationMessage> conversationMessageObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+    .get(ref);
+```
+Returns a single `ConversationMessage`.
+
+## List Message Headers of a `Conversation`
+```Java
+Observable<List<ConversationMessageHeader>> conversationMessageHeadersObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+    .listHeaders(ref);
+```
+Returns a single `List`.
+
+## List `ConversationMessage`s
+```Java
+Observable<List<ConversationMessage>> conversationMessagesObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+    .listMessages(ref);
+```
+Returns a single `List`.
+
+## Delete a `ConversationMessage`
+```Java
+Observable<Boolean> resultObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+    .delete(ref);
+```
+Returns a single `Boolean`.
+
+## Listen to live create `ConversationMessage`
+```Java
+Observable<ConversationMessage> conversationMessagesObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+    .listen(ref);
+```
+Returns 0 or more `ConversationMessage`, but never calls `.onCompleted()`.
+It is your responsibility to call `subscription.unsubscribe()` , as this triggers the `removeListener` method, in order to avoid memory leakage.
--- app/src/main/java/com/github/xavierlepretre/rxqredo/MainActivity.java
+++ app/src/main/java/com/github/xavierlepretre/rxqredo/MainActivity.java
@@ -1,12 +1,23 @@
 package com.github.xavierlepretre.rxqredo;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.qredo.device.android.QredoClient;
 import com.qredo.device.android.QredoClientAdapter;
 import com.qredo.device.android.QredoClientObservable;
+import com.qredo.device.android.conversation.Conversation;
+import com.qredo.device.android.conversation.ConversationManagerObservable;
+import com.qredo.device.android.conversation.ConversationRef;
+import com.qredo.device.android.conversationmessage.ConversationMessage;
+import com.qredo.device.android.conversationmessage.ConversationMessageHeader;
+import com.qredo.device.android.conversationmessage.ConversationMessageManagerObservable;
+import com.qredo.device.android.conversationmessage.ConversationMessageRef;
+import com.qredo.device.android.rendezvous.Rendezvous;
+import com.qredo.device.android.rendezvous.RendezvousCreationParams;
+import com.qredo.device.android.rendezvous.RendezvousManagerObservable;
+import com.qredo.device.android.rendezvous.RendezvousRef;
 import com.qredo.device.android.vault.VaultItem;
 import com.qredo.device.android.vault.VaultItemHeader;
 import com.qredo.device.android.vault.VaultItemRef;
@@ -62,6 +73,7 @@ public class MainActivity extends AppCompatActivity
                         });
     }
 
+    //<editor-fold desc="Vault Examples">
     private void examplePutVaultItem(@NonNull QredoClient qredoClient, @NonNull VaultItem item)
     {
         Observable<VaultItemRef> itemObservable = new VaultManagerObservable(qredoClient.getVaultManager())
@@ -109,6 +121,126 @@ public class MainActivity extends AppCompatActivity
         Observable<Set<VaultItemHeader>> headersObservable = new VaultManagerObservable(qredoClient.getVaultManager())
                 .findHeaders(matcher);
     }
+    //</editor-fold>
+
+    //<editor-fold desc="Rendezvous Examples">
+    private void exampleCreateRendezvous(@NonNull QredoClient qredoClient, @NonNull RendezvousCreationParams params)
+    {
+        Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .create(params);
+    }
+
+    private void exampleRespondRendezvous(@NonNull QredoClient qredoClient, @NonNull String tag)
+    {
+        Observable<ConversationRef> conversationRefObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .respond(tag);
+    }
+
+    private void exampleGetRendezvousByRef(@NonNull QredoClient qredoClient, @NonNull RendezvousRef ref)
+    {
+        Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .get(ref);
+    }
+
+    private void exampleActivateRendezvousByTag(@NonNull QredoClient qredoClient, @NonNull RendezvousRef ref, int duration)
+    {
+        Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .activate(ref, duration);
+    }
+
+    private void exampleDeactivateRendezvousByTag(@NonNull QredoClient qredoClient, @NonNull RendezvousRef ref)
+    {
+        Observable<Boolean> resultObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .deactivate(ref);
+    }
+    private void exampleListRendezvous(@NonNull QredoClient qredoClient)
+    {
+        Observable<Set<Rendezvous>> rendezvousesObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .list();
+    }
+
+    private void exampleListenToLiveRendezvous(@NonNull QredoClient qredoClient)
+    {
+        Observable<Rendezvous> rendezvousObservable = new RendezvousManagerObservable(qredoClient.getRendezvousManager())
+                .listen();
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Conversation Examples">
+    private void exampleListConversationsByRef(@NonNull QredoClient qredoClient, @NonNull RendezvousRef ref)
+    {
+        Observable<Set<Conversation>> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .list(ref);
+    }
+
+    private void exampleListConversationsByTag(@NonNull QredoClient qredoClient, @NonNull String tag)
+    {
+        Observable<Set<Conversation>> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .list(tag);
+    }
+
+    private void exampleListAllConversations(@NonNull QredoClient qredoClient)
+    {
+        Observable<Set<Conversation>> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .listAll();
+    }
+
+    private void exampleGetConversations(@NonNull QredoClient qredoClient, @NonNull ConversationRef ref)
+    {
+        Observable<Conversation> conversationObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .get(ref);
+    }
+
+    private void exampleDeleteConversations(@NonNull QredoClient qredoClient, @NonNull ConversationRef ref)
+    {
+        Observable<Boolean> resultObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .delete(ref);
+    }
+
+    private void exampleLeaveConversations(@NonNull QredoClient qredoClient, @NonNull ConversationRef ref)
+    {
+        Observable<Boolean> resultObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .leave(ref);
+    }
+
+    private void exampleListenToLiveCreatedConversations(@NonNull QredoClient qredoClient, @NonNull RendezvousRef ref)
+    {
+        Observable<Conversation> conversationsObservable = new ConversationManagerObservable(qredoClient.getConversationManager())
+                .listen(ref);
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="ConversationMessage Examples">
+    private void exampleGetConversationMessage(@NonNull QredoClient qredoClient, @NonNull ConversationMessageRef ref)
+    {
+        Observable<ConversationMessage> conversationMessageObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+                .get(ref);
+    }
+
+    private void exampleListConversationMessageHeaders(@NonNull QredoClient qredoClient, @NonNull ConversationRef ref)
+    {
+        Observable<List<ConversationMessageHeader>> conversationMessageHeadersObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+                .listHeaders(ref);
+    }
+
+    private void exampleListConversationMessages(@NonNull QredoClient qredoClient, @NonNull ConversationRef ref)
+    {
+        Observable<List<ConversationMessage>> conversationMessagesObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+                .listMessages(ref);
+    }
+
+    private void exampleDeleteConversationMessages(@NonNull QredoClient qredoClient, @NonNull ConversationMessageRef ref)
+    {
+        Observable<Boolean> resultObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+                .delete(ref);
+    }
+
+    private void exampleListenToLiveConversationMessages(@NonNull QredoClient qredoClient, @NonNull ConversationRef ref)
+    {
+        Observable<ConversationMessage> conversationMessagesObservable = new ConversationMessageManagerObservable(qredoClient.getConversationMessageManager())
+                .listen(ref);
+    }
+    //</editor-fold>
 
     @Override protected void onDestroy()
     {
